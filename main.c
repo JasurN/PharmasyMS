2de6827b465fa8e5feb14d3acca1fc70b7e12e56
BMWm3f80 <kshohrux@mail.ru>
2017-12-06 17:12:03 +0500

    Modified glade file

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <gtk/gtk.h>
+#include <string.h>
+#define username "admin"
 
 GtkBuilder *builder;
 GtkWidget *window, *window1;
+
+
 int main(int argc, char **argv)
 {
 
@@ -15,23 +19,28 @@ int main(int argc, char **argv)
    }
 
    window=GTK_WIDGET( gtk_builder_get_object(builder, "logwindow"));
-
    gtk_builder_connect_signals(builder, NULL);
    gtk_widget_show_all(window);
    gtk_main();
    return (0);
 }
 void on_enter_but_clicked(){
-   const gchar* login;
-   const gchar* password;
+   const char* login;
+   const char* password;
 
    GtkEntry* log = (GtkEntry*) gtk_builder_get_object(builder, "login_field");
    GtkEntry* pass=(GtkEntry*) gtk_builder_get_object(builder, "password_field");
    login=gtk_entry_get_text(log);
    password=gtk_entry_get_text(pass);
-   window1=GTK_WIDGET( gtk_builder_get_object(builder, "window1"));
-   gtk_widget_show_all(window1);
-   g_print(login);
-   g_print(password);
 
+   if (strcmp(login, username)==0 && strcmp(password, username)==0){
+      window1=GTK_WIDGET( gtk_builder_get_object(builder, "window1"));
+      gtk_widget_show_all(window1);
+      gtk_widget_destroy(window);
+      g_print(login);
+      g_print(password);
+   } else{
+      g_print("Wrong");
+
+   }
 }
