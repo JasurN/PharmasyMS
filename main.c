c654a099a89d81b252b71c6520fcb77fb4a66c77
BMWm3f80 <kshohrux@mail.ru>
2017-12-17 14:16:42 +0500

    Login page completed

--- main.c
+++ main.c
@@ -4,8 +4,16 @@
 
 GtkBuilder *builder;
 GtkWidget *window, *window1;
-GtkLabel *warnlabel;
+GtkCellRenderer *renderer;
+GtkTreeViewColumn *col;
 
+enum {
+    LIST_ID,
+    LIST_BRAND,
+    LIST_MODEL,
+    LIST_PRICE,
+    N_COLUMNS
+};
 
 int main(int argc, char **argv)
 {
@@ -38,9 +46,19 @@ void on_enter_but_clicked(){
    GtkEntry* pass=(GtkEntry*) gtk_builder_get_object(builder, "password_field");
    login=gtk_entry_get_text(log);
    password=gtk_entry_get_text(pass);
-
+    char *name="OxyMed";
    if (strcmp(login, username)==0 && strcmp(password, username)==0){
+
       window1=GTK_WIDGET( gtk_builder_get_object(builder, "window1"));
+       GtkLabel* storename=(GtkLabel*) gtk_builder_get_object(builder, "drugstore");
+       gtk_label_set_text(GTK_LABEL(storename), name);
+       GtkTreeView* list=(GtkTreeView*) gtk_builder_get_object(builder,"table1");
+       renderer=gtk_cell_renderer_text_new();
+       col=gtk_tree_view_column_new_with_attributes("ID", renderer,"text",LIST_ID,NULL);
+       gtk_tree_view_append_column(GTK_TREE_VIEW(list), col);
+
+
+
       gtk_widget_show_all(window1);
       gtk_widget_destroy(window);
       g_print(login);
